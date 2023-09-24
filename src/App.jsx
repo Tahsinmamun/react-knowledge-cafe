78f2696c2c6d2c1e8fd994dc9e4a1019213816ac
Tahsinmamun <chowdhurymamun097@gmail.com>
2023-09-24 13:05:28 +0600

    Loaded Data

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import Blogs from './Components/Blogs/Blogs'
+import Header from './Components/Header/Header'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <div>
+        <Header></Header>
+        <Blogs></Blogs>
+      </div>
+     
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h3 className='text-4xl font-bold'>Knowlege Cafe</h3>
+            
+        </div>
+    );
+};
+
+export default Header;
